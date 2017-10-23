3a827a755b0bb8c7b062738847f069681aab7932
周旭鹏 <731299128@qq.com>
2017-10-23 15:49:02 +0800

    新添加的100内的和

---
+++ src/zhouxupeng/zhouxupeng2.java
@@ -0,0 +1,16 @@
+package zhouxupeng;
+
+public class zhouxupeng2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int sum = 0;	
+		    for(int i=1;i<=100;i++) {
+			if (i % 3 != 0) {
+			   sum = i+sum;	
+			}	
+			System.out.println(sum);
+		       
+	}
+	}}
+
